ee06bbb09358560f3acc29ed8fb8660cebcfd1c2
Austin Peel <austinpeel@gmail.com>
2022-02-15 07:28:32 -0600

    Add warning message about app aspect ratio problem

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import Header from '../Header/Header';
 import Title from '../Title/Title';
 import AppNav from '../AppNav/AppNav';
 import Main from '../Main/Main';
+import Message from '../Message/Message';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <Title />
       <AppNav />
+      <Message />
       <Main />
     </div>
   );
---
+++ src/components/Message/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Message.css';
+
+export default function Message() {
+  return (
+    <p className='message'>
+      Resize the browser window slightly if the apps look stretched vertically.
+      This can happen when first loading the page.
+    </p>
+  );
+}
