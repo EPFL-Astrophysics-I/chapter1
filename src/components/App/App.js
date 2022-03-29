0fe7167234682aa96c48f806792f5623701e6d3d
Austin Peel <austinpeel@gmail.com>
2022-03-29 10:21:42 +0200

    Add GA tracking. Update manifest

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import Header from '../Header/Header';
 import Title from '../Title/Title';
 import AppNav from '../AppNav/AppNav';
 import Main from '../Main/Main';
-import Message from '../Message/Message';
+// import Message from '../Message/Message';
 import Footer from '../Footer/Footer';
 
 export default function App() {
